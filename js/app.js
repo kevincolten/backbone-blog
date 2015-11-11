4e5037d5b26f1b49e13ce977dd4fca1c9d610416
mistakevin <kevincolten@gmail.com>
2015-11-11 07:15:53 -0600

    list loading

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-//
+'use strict';
+
+var Backbone = require('backbone');
+var $ = require('jquery');
+
+var PostModel = Backbone.Model.extend({});
+
+var PostCollection = Backbone.Collection.extend({
+    url: 'http://localhost:8080/api/gists.json',
+
+    model: PostModel
+});
+
+var posts = new PostCollection();
+
+posts.fetch({
+    success: function() {
+        var postsListView = new PostsListView({ collection: posts });
+        postsListView.render();
+        $('#content').html(postsListView.el);
+    }
+});
+
+var PostsListView = Backbone.View.extend({
+    el: '<ul></ul>',
+
+    render: function() {
+        var that = this;
+        this.collection.each(function (post) {
+           that.$el.append('<li>' + post.get('description') + '</li>');
+        });
+    }
+});
