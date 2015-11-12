010a41fae14007adb181cc87821b92ab1cd46101
Chen Yang <yangc5@mail.uc.edu>
2015-11-11 18:35:01 -0600

    backbone to_do list

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-//
+'use strict';
+
+var Backbone = require('backbone');
+var $= require('jquery');
+
+var PostModel = Backbone.Model.extend({});
+var PostsCollection = Backbone.Collection.extend({
+  url: '../api/gists.json',
+  model: PostModel
+});
+var posts = new PostsCollection();
+posts.fetch({
+  success: function(){
+    var postsListView = new PostsListView({collection: posts});
+    postsListView.render();
+    $('#content').html(postsListView.el);
+  }
+});
+var PostsListView = Backbone.View.extend({
+  el: '<ul></ul>',
+  render: function(){
+    var that = this;
+    this.collection.each(function(post){
+      that.$el.append('<li>'+ post.get('description')+'</li>');
+    });
+  }
+});
