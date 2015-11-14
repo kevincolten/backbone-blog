89f9175769171d64f96ea3c1ba96b4d2330423bd
Chen Yang <yangc5@mail.uc.edu>
2015-11-14 11:58:14 -0600

    stop point

--- js/app.js
+++ js/app.js
@@ -1,27 +1,15 @@
 'use strict';
 
-var Backbone = require('backbone');
-var $= require('jquery');
+var $ = require('jquery');
+var PostsListView = require('./views/PostsListView.js');
+var PostsCollection = require('./collections/PostsCollection.js');
+var AppRouter = require('./AppRouter.js');
 
-var PostModel = Backbone.Model.extend({});
-var PostsCollection = Backbone.Collection.extend({
-  url: '../api/gists.json',
-  model: PostModel
-});
-var posts = new PostsCollection();
-posts.fetch({
-  success: function(){
-    var postsListView = new PostsListView({collection: posts});
-    postsListView.render();
-    $('#content').html(postsListView.el);
-  }
-});
-var PostsListView = Backbone.View.extend({
-  el: '<ul></ul>',
-  render: function(){
-    var that = this;
-    this.collection.each(function(post){
-      that.$el.append('<li>'+ post.get('description')+'</li>');
-    });
-  }
+
+$(function(){
+  // var posts = new PostsCollection();
+  new AppRouter();
+
+  Backbone.history.
+
 });
---
+++ js/collections/PostsCollection.js
@@ -0,0 +1,10 @@
+var Backbone = require('backbone');
+var PostModel = require('../models/PostModel.js');
+
+var PostsCollection = Backbone.Collection.extend({
+  url: '../../api/gists.json',
+
+  model: PostModel
+});
+
+module.exports=PostsCollection;
